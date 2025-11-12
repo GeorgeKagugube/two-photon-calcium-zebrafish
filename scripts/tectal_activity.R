769bfb257ad061ddcd077ef8839fd7ae233816bd
GeorgeKagugube <ggrwilliam@yahoo.co.uk>
2025-11-12 18:13:50 +0000

    Added an r script to extract the tectal neuronal activity

---
+++ scripts/tectal_activity.R
@@ -0,0 +1,28 @@
+## Clear the workspce here 
+rm(list = ls())
+
+## Set a global seed here for reproducibility
+set.seed(101)
+
+## Set the working directory here 
+setwd('/Users/gwk/Desktop/Bioinformatics/two-photon-calcium-zebrafish/scripts/out_mn_pipeline')
+## Source any extarnal files that maybe needed to complete the tasks here 
+
+## Optional, but you can check the contents of the directory to be sure that the file of interest
+# is located here 
+dir()
+
+## Load the modules that are needed for the analysis here 
+# Load required packages
+library(ggplot2)
+library(ggsignif)
+library(gridExtra)
+library(tidyverse)
+library(ggpubr)
+library(patchwork)
+
+## Load the data to be analysed here
+
+
+
+
